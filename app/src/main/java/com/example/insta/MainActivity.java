394ccc8b031b19c79eba027c9da65b0852d0eb0f
Ivana <ivana.zhangyf@gmail.com>
2019-01-19 10:04:58 -0500

    Create a new activity for Register

--- app/src/main/java/com/example/insta/MainActivity.java
+++ app/src/main/java/com/example/insta/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.insta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void register(View view) {
+
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+
     }
 }
