b7f3622ae90540e1bfcdaa890a7b74fd258e5a29
Ivana <ivana.zhangyf@gmail.com>
2019-01-19 10:44:03 -0500

    Update profile page UI

--- app/src/main/java/com/example/insta/Register.java
+++ app/src/main/java/com/example/insta/Register.java
@@ -1,7 +1,9 @@
 package com.example.insta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
